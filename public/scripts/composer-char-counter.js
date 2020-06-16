d524ffcc1d957f725959bfd9689bc2fa1b637f4e
Jessica Seo <jessicaseo83@gmail.com>
2020-06-16 19:28:27 +0000

    Implement character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $("#tweet-text").keyup(function() {
+    $(this)
+      .closest(".new-tweet")
+      .find(".counter")
+      .text(140 - $(this).val().length);
+
+    const max = 140;
+    const length = $(this).val().length;
+    if(max - length < 0) {
+      $(".counter").css('color', 'red')
+    }
+
+  })
+
+});
+
+  
