0a21e6eb57053658197c9e47ef09e8af77cc93b2
Jessica Seo <jessicaseo83@gmail.com>
2020-06-18 02:16:17 +0000

    Implement validation display

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,34 +6,42 @@
 
 
 
-const data = [{
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png"
-    ,
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-},
-{
-  "user": {
-    "name": "Descartes",
-    "avatars": "https://i.imgur.com/nlhLi3I.png",
-    "handle": "@rd" },
-  "content": {
-    "text": "Je pense , donc je suis"
-  },
-  "created_at": 1461113959088
-}]
+// const data = [{
+//   "user": {
+//     "name": "Newton",
+//     "avatars": "https://i.imgur.com/73hZDYK.png"
+//     ,
+//     "handle": "@SirIsaac"
+//   },
+//   "content": {
+//     "text": "If I have seen further it is by standing on the shoulders of giants"
+//   },
+//   "created_at": 1461116232227
+// },
+// {
+//   "user": {
+//     "name": "Descartes",
+//     "avatars": "https://i.imgur.com/nlhLi3I.png",
+//     "handle": "@rd" },
+//   "content": {
+//     "text": "Je pense , donc je suis"
+//   },
+//   "created_at": 1461113959088
+// }]
+
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
 
 const renderTweets = function(tweets) {
+  $('.article-tweet').empty();
   for(let tweet of tweets) {
-    $('.article-tweet').append(createTweetElement(tweet))
+    $('.article-tweet').prepend(createTweetElement(tweet))
   }
 }
+
 
 const createTweetElement = function(tweet) {
 
@@ -44,10 +52,10 @@ const createTweetElement = function(tweet) {
       <div class="handle">${tweet.user.handle}</div>
     </header>
     <div class="tweet">
-      ${tweet.content.text}
+      <p>${escape(tweet.content.text)}</p>
     </div>
     <footer>
-      <div>10 days ago</div>
+      <div>${moment(tweet.created_at, "").fromNow()}</div>
       <div class="btns"><img src="/images/flag.png"><img src="/images/retweet.png"><img src="/images/heart.png"></div>
     </footer>
   </article>
@@ -69,15 +77,25 @@ $(document).ready(function () {
     
   
     if (input.length > 140) {
-      alert("It's too long!");
+      // alert("It's too long!");
+  
+      $(".error").replaceWith("<div class='error'>⛔️ It's too long! (Max 140 Character) ⛔️</div>")
+      $(".error").slideDown();
       return;
+
     } else if (input === "") {
-      alert("It's too short!");
+     
+      $(".error").replaceWith("<div class='error'>⛔️ It's too short! Humming more! ⛔️</div>")
+      $(".error").slideDown();
       return;
+
     } else if (input === null) {
-      alert("I don't know what you want to say. Please try again!");
+      
+      $(".error").replaceWith("<div class='error'>⛔️ I don't know what you want to say. Please try again! ⛔️</div>")
+      $(".error").slideDown();
       return;
     } else {
+      $(".error").slideUp();
       $.ajax({
         url: '/tweets',
         method: 'POST',
