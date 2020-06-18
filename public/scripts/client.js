2d076ccaf00f8fdce251f74f1a34af0963dd12cd
Jessica Seo <jessicaseo83@gmail.com>
2020-06-18 22:23:48 +0000

    Delete hard coded data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,29 +6,8 @@
 
 
 
-// const data = [{
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png"
-//     ,
-//     "handle": "@SirIsaac"
-//   },
-//   "content": {
-//     "text": "If I have seen further it is by standing on the shoulders of giants"
-//   },
-//   "created_at": 1461116232227
-// },
-// {
-//   "user": {
-//     "name": "Descartes",
-//     "avatars": "https://i.imgur.com/nlhLi3I.png",
-//     "handle": "@rd" },
-//   "content": {
-//     "text": "Je pense , donc je suis"
-//   },
-//   "created_at": 1461113959088
-// }]
 
+//
 const escape =  function(str) {
   let div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
@@ -101,7 +80,8 @@ $(document).ready(function () {
         method: 'POST',
         data: $(this).serialize()
       }).then(function(data) {
-        console.log('Submitted')
+        $("textarea").val('');
+        $(".counter").val(140);
         loadTweets();
       })
     }
